b9f5b3484e163e34e71bd8c49b3b671b6b9ababc
DolgikhNikita <dolgih.nikita59@gmail.com>
2023-03-30 22:39:02 +0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Никита Долгих on 05/03/2023.
+//
+
+import UIKit
+
+@main
+class CalculatorApp: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
